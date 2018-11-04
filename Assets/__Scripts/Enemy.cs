b8857c489d2643bea930830f59b4f1f8a8b43f97
TylerBrick <35938639+whothere788@users.noreply.github.com>
2018-11-03 20:06:53 -0400

    close to first draft

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [Header(" Set in Inspector: Enemy")]
+    public float speed = 10f;    
+    public float fireRate = 0.3f;   
+    public float health = 10;
+    public int score = 100;
+    private BoundsCheck bndCheck;
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    public Vector3 pos {
+    get {
+            return ( this.transform.position );
+        }
+        set {
+            this.transform.position = value;
+        }
+    }
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if(bndCheck != null && bndCheck.offDown)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
---
+++ Assets/__Scripts/Hero.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+    static public Hero S;
+    [Header("Set in Inspector")]
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake()
+    {
+        if(S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");  
+        }
+    }
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        print("Triggered: "+ go.name);
+        
+    }
+}
